c51b7d2367aea25244b75c7b995d7eb1c23d0889
endered <yy56ga10ve@gmail.com>
2021-10-24 00:21:16 +0900

    add router route

--- src/router/router.go
+++ src/router/router.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jphacks/A_2108/src/controller"
 )
 
 func Route() {
 	r := gin.Default()
+	con := controller.NewController()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	r.GET("/fire", con.Fire1)
 	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		fmt.Printf("%#v\n", err)
