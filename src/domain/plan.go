f99577773b225310edd6fc91c653c39c09f2f070
endered <yy56ga10ve@gmail.com>
2021-10-24 05:11:20 +0900

    add DB side definition

--- src/domain/plan.go
+++ src/domain/plan.go
@@ -1,9 +1,59 @@
 package domain
 
+import "time"
+
 type DBPlan struct {
 	ID          int
 	Title       string
 	Description string
 	Image       string
-	CreatorID   string // CreatorのID
+	CreatorID   int // DBUser.ID
+}
+
+type DBCondition struct {
+	ID     int
+	PlanID int // DBPlan.ID
+}
+
+type DBSeason struct {
+	ID          int
+	ConditionID int // DBCondition.ID
+	// TODO
+}
+
+type DBTimeSpan struct {
+	ID          int
+	ConditionID int // DBCondition.ID
+	// TODO
+}
+
+type DBCategory struct {
+	ID          int
+	ConditionID int // DBCondition.ID
+	// TODO
+}
+
+type DBDay struct {
+	ID     int
+	PlanID int // DBPlan.ID
+	NthDay int
+}
+
+type DBHeading struct {
+	ID          int
+	DayID       int // DBDay.ID
+	HeadingText string
+	Order       int
+}
+
+type DBSchedule struct {
+	ID              int
+	DayID           int // DBDay.ID
+	Description     string
+	StartTime       time.Time
+	EndTime         time.Time
+	Place           int
+	HPLink          string
+	ReservationLink string
+	Order           int
 }
