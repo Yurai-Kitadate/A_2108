39e0322e62ab6469fa2edf0f132083b0b8766c66
endered <yy56ga10ve@gmail.com>
2021-10-24 00:17:54 +0900

    add controller definition

---
+++ src/controller/controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jphacks/A_2108/src/domain"
+)
+
+type Controller struct {
+}
+
+func (con *Controller) Fire1(c *gin.Context) {
+	res := domain.Fire1{
+		A: 1,
+		B: "mieruka?",
+		C: "mienaiyo",
+	}
+	c.JSON(200, res)
+}
