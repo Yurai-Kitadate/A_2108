25fbd1cc8b0210c219c4c8e4c7b2b5c27c0f5735
Lightbells <takahasi0928@gmail.com>
2021-10-25 16:46:19 +0900

    [Fix]: Add test configure.

--- src/config/database.go
+++ src/config/database.go
@@ -1,13 +1,35 @@
 package config
 
+import (
+	"os"
+)
+
+func isTest() bool {
+	env_isTest := os.Getenv("IS_TEST")
+	if env_isTest == "" {
+		return false
+	}
+	return true
+}
+
 func DBUser() string {
+	if (isTest()) {
+		return "root"
+	}
+
 	return "TODO"
 }
 
 func DBPass() string {
+	if (isTest()){
+		return "De3thM3rch"
+	}
 	return "TODO"
 }
 
 func DBMethod() string {
+	if (isTest()){
+		return "tcp(localhost:3306)"
+	}
 	return "TODO"
 }
