09ee1a71c89cac1c360dfe96c93bac681343495d
shinbunbun <takamuneyuto@gmail.com>
2021-10-25 06:16:21 +0000

    move user to domain

--- src/controller/user.go
+++ src/controller/user.go
@@ -6,41 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type contacts struct {
-	ID        int    `json:"id"`
-	Hp        string `json:"hp"`
-	Instagram string `json:"instagram"`
-	Twitter   string `json:"twitter"`
-	Facebook  string `json:"facebook"`
-	Tiktok    string `json:"tiktok"`
-	Biography string `json:"biography"`
-}
-type job struct {
-	ID             int       `json:"id"`
-	Jobname        string    `json:"jobname"`
-	Dateoffirstjob time.Time `json:"dateoffirstjob"`
-}
-type address struct {
-	ID          int    `json:"id"`
-	Area        string `json:"area"`
-	Prefecture  string `json:"prefecture"`
-	City        string `json:"city"`
-	Description string `json:"description"`
-}
-type user struct {
-	ID          int       `json:"id"`
-	UserName    string    `json:"userName"`
-	Image       string    `json:"image"`
-	Email       string    `json:"email"`
-	DisplayName string    `json:"displayName"`
-	Birthday    time.Time `json:"birthday"`
-	Sex         string    `json:"sex"`
-	Contacts    contacts  `json:"contacts"`
-	Creator     creator   `json:"creator"`
-	Job         job       `json:"job"`
-	Address     address   `json:"address"`
-}
-
 func (con *Controller) UserGet(c *gin.Context) {
 	res := user{
 		ID:          0,
