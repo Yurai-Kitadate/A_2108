d8b16258ad6f11789da82f723691cbd647a58dbe
endered <yy56ga10ve@gmail.com>
2021-10-28 12:35:03 +0900

    add mock get user by id

---
+++ src/controller/mock.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jphacks/A_2108/src/domain"
+)
+
+func MockGetUserByID(c *gin.Context) {
+	planId := c.Param("id")
+	planIdInt, err := strconv.Atoi(planId)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"Error": "Atoi error: " + err.Error(),
+		})
+		return
+	}
+
+	var res domain.User
+	if planIdInt == 1 {
+		res = MockUser1
+	} else if planIdInt == 2 {
+		res = MockUser2
+	} else {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"Error": "Not Found",
+		})
+	}
+	c.JSON(200, res)
+}
