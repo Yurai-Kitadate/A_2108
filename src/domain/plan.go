4fd8a64585602eab999a8013ca6ba1c68c420b60
Lightbells <takahasi0928@gmail.com>
2021-10-29 05:31:02 +0900

    型修正

--- src/domain/plan.go
+++ src/domain/plan.go
@@ -61,5 +61,5 @@ type Conditions struct {
 	Season        []Season   `json:"season"`
 	TimeSpan      []TimeSpan `json:"timeSpan"`
 	Category      []Category `json:"category"`
-	EstimatedCost int        `json:"estimatedcost"`
+	EstimatedCost int        `json:"int"`
 }
