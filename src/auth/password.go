b7bc83288abcd36cce934837ed38dbe5f5d286d0
shinbunbun <takamuneyuto@gmail.com>
2021-10-28 20:29:04 +0000

    add verify password

---
+++ src/auth/password.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"encoding/hex"
+	"errors"
+
+	"golang.org/x/crypto/sha3"
+)
+
+func VerifyPassword(password, email, storedHash string) error {
+	// Hash生成
+	hash := sha3.New512()
+	_, err := hash.Write([]byte(password))
+	if err != nil {
+		return err
+	}
+	// 検証
+	if hex.EncodeToString(hash.Sum(nil)) != storedHash {
+		return errors.New("Invalid password")
+	}
+	// 結果返却
+	return nil
+}
