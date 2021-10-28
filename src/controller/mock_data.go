04780725fdf6462db58e292f3ec703934c31a04a
endered <yy56ga10ve@gmail.com>
2021-10-28 12:26:34 +0900

    add plan data

--- src/controller/mock_data.go
+++ src/controller/mock_data.go
@@ -57,4 +57,86 @@ func init() {
 			},
 		}
 	}
+	{
+		MockPlan.PlanId = 1
+		MockPlan.Title = "のんびり会津旅"
+		MockPlan.Description = "のんびりと会津の旅行を楽しむ"
+		MockPlan.Image = "https://avatars.githubusercontent.com/u/34409044?s=40&v=4"
+		MockPlan.CreatedAt = time.Now()
+		MockPlan.CreatorUser = MockUser2
+		hplink := "https://twitter.com/home"
+		reservelink := "https://twitter.com/home"
+		MockPlan.Days = domain.Days{
+			{
+				NthDay: 1,
+				Headings: domain.Headings{
+					{
+						ID:    1,
+						Text:  "今日一番目の予定",
+						Order: 1,
+					},
+					{
+						ID:    2,
+						Text:  "今日2番目の予定",
+						Order: 3,
+					},
+					{
+						ID:    3,
+						Text:  "今日参番目の予定",
+						Order: 5,
+					},
+				},
+				Schedule: domain.Schedule{
+					{
+						ID:          1,
+						Title:       "電車に乗る",
+						Description: "遠くに移動するので電車に乗る",
+						StartTime:   420,
+						EndTime:     435,
+						Addresss: &domain.Address{
+							ID:       1,
+							PlusCode: "MP43+V5 渋谷区",
+						},
+						HpLink:          &hplink,
+						ReservationLink: &reservelink,
+						Order:           4,
+					},
+				},
+			},
+		}
+		MockPlan.Conditions = domain.Conditions{
+			ID: 123,
+			Place: []domain.Place{
+				{
+					ID:         1,
+					Area:       4,
+					Prefecture: 3,
+					City:       2,
+					Name:       "doko?koko",
+				},
+			},
+			Season: []domain.Season{
+				{
+					ID:   5,
+					Text: "夏",
+				},
+			},
+			TimeSpan: []domain.TimeSpan{
+				{
+					ID:   1,
+					Text: "1月",
+				},
+			},
+			Category: []domain.Category{
+				{
+					ID:   1,
+					Text: "娯楽",
+				},
+			},
+		}
+		MockPlans = make(domain.Plans, 0)
+		for i := 1; i <= 10; i++ {
+			MockPlans = append(MockPlans, MockPlan)
+		}
+	}
 }
