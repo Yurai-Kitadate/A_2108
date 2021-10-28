2d51b5b50340261d5554542f1683b41df661974c
shinbunbun <takamuneyuto@gmail.com>
2021-10-28 14:30:25 +0000

    move api

--- src/router/router.go
+++ src/router/router.go
@@ -9,6 +9,7 @@ import (
 func Route() *gin.Engine {
 	r := gin.Default()
 	con := controller.NewControllerWithYesmanRepository()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -21,18 +22,18 @@ func Route() *gin.Engine {
 	authGroup := r.Group("/", auth.VerifyAPIMiddleware())
 	{
 		authGroup.GET("/user", con.UserGet)
-
-		authGroup.GET("/user/:id", con.GetUserByID)
 		authGroup.POST("/user", con.CreateUser)
 		authGroup.PUT("/user", con.UpdateUser)
 		authGroup.DELETE("/user/:id", con.DeleteUser)
 
-		authGroup.GET("/plan", con.GetAllPlans)
-		authGroup.GET("/plan/:id", con.GetPlanByID)
 		authGroup.POST("/plan", con.CreatePlan)
 		authGroup.PUT("/plan", con.UpdatePlan)
 		authGroup.DELETE("/plan/:id", con.DeletePlanByID)
 	}
+
+	r.GET("/user/:id", con.GetUserByID)
+	r.GET("/plan", con.GetAllPlans)
+	r.GET("/plan/:id", con.GetPlanByID)
 
 	r.POST("/register", con.RegisterPost)
 	r.POST("/login", con.LoginPOST)
