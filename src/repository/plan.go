df64c243b1e04397900aec7fdc104023f6cabe81
Lightbells <takahasi0928@gmail.com>
2021-10-29 21:03:52 +0000

    /planの全権取得を実装

--- src/repository/plan.go
+++ src/repository/plan.go
@@ -262,7 +262,22 @@ func (pr PlanRepository) GetPlanByID(planID int) (domain.Plan, error) {
 }
 
 func (pr PlanRepository) GetPlansOrderedbyTime(limit int) (domain.Plans, error) {
+	db := pr.db
+	var plans domain.Plans
+	db_plans := []domain.DBPlan{}
 
+	err := db.Limit(limit).Find(&db_plans).Error
+	if err != nil {
+		return domain.Plans{}, errHandling(err)
+	}
+
+	plans = make(domain.Plans, len(db_plans))
+	for i, v := range db_plans {
+		plans[i], err = pr.GetPlanByID(v.ID)
+		if err != nil {
+			return plans, errHandling(err)
+		}
+	}
 	return nil, nil
 }
 
