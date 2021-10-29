8a7a28f6b7fad17b67ab9700e2b29226922c8f7e
shinbunbun <takamuneyuto@gmail.com>
2021-10-29 15:46:42 +0000

    fix GetPlace

--- src/controller/place.go
+++ src/controller/place.go
@@ -8,26 +8,25 @@ import (
 )
 
 func (con *Controller) GetPlace(c *gin.Context) {
+	intArea := 0
+	intPref := 0
+
 	area, ok := c.GetQuery("area")
-	if !ok {
-		c.JSON(400, gin.H{"error": "area is required"})
-		return
+	var err error
+	if ok {
+		intArea, err = strconv.Atoi(area)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "area is invalid"})
+			return
+		}
 	}
 	pref, ok := c.GetQuery("pref")
-	if !ok {
-		c.JSON(400, gin.H{"error": "pref is required"})
-		return
-	}
-
-	intArea, err := strconv.Atoi(area)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "area is invalid"})
-		return
-	}
-	intPref, err := strconv.Atoi(pref)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "pref is invalid"})
-		return
+	if ok {
+		intPref, err = strconv.Atoi(pref)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "pref is invalid"})
+			return
+		}
 	}
 
 	place := place.GetPlace(intArea, intPref)
