ff9ec7ce1ccdc1830d724d341df1cb64ab9e4f92
shinbunbun <takamuneyuto@gmail.com>
2021-10-29 14:42:49 +0000

    create getAPIResponse

---
+++ src/area/api.go
@@ -0,0 +1,41 @@
+package area
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type response struct {
+	Status string `json:"status"`
+	Data   []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"data"`
+}
+
+func getAPIResponse(pref int) (response, error) {
+	queryArea := fmt.Sprintf("%2d", pref)
+	url := "https://www.land.mlit.go.jp/webland/api/CitySearch?area=" + queryArea
+	resp, err := http.Get(url)
+	if err != nil {
+		return response{}, err
+	}
+	defer resp.Body.Close()
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return response{}, err
+	}
+
+	jsonBytes := ([]byte)(byteArray)
+	data := new(response)
+
+	if err := json.Unmarshal(jsonBytes, data); err != nil {
+		fmt.Println("JSON Unmarshal error:", err)
+		return response{}, err
+	}
+
+	return *data, nil
+}
