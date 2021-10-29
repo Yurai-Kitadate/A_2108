d9e192a8176d1bb7606d4489ddd0088bed8a6050
endered <yy56ga10ve@gmail.com>
2021-10-29 13:51:24 +0900

    add image repository to controller definition

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,8 +1,9 @@
 package controller
 
 type Controller struct {
-	UserRepository UserRepository
-	PlanRepository PlanRepository
+	UserRepository  UserRepository
+	PlanRepository  PlanRepository
+	ImageRepository ImageRepository
 }
 
 func NewController() *Controller {
