1767895859ca000d914ed80e47f59ff54428bd03
endered <yy56ga10ve@gmail.com>
2021-10-29 15:58:08 +0900

    add image function definition

--- src/controller/image.go
+++ src/controller/image.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jphacks/A_2108/src/auth"
 	"github.com/jphacks/A_2108/src/domain"
 	"github.com/jphacks/A_2108/src/s3"
 )
@@ -22,7 +23,11 @@ func (con *Controller) ImagePost(c *gin.Context) {
 		return
 	}
 	// TODO: IDの取得を実装
-	id := 100
+	id, err := auth.GetIdBySession(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	sess := s3.NewS3Session()
 	endpoint, err := s3.UploadToS3(sess, body, strconv.Itoa(id))
 	if err != nil {
@@ -30,4 +35,18 @@ func (con *Controller) ImagePost(c *gin.Context) {
 		return
 	}
 	c.JSON(200, endpoint)
+}
+
+func (con *Controller) GetOwnImages(c *gin.Context) {
+	id, err := auth.GetIdBySession(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	images, err := con.ImageRepository.GetImagesByUserID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, images)
 }
