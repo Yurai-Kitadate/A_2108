a6901dcdfb8c92f2a5873d55f7049eb1c20325ed
endered <yy56ga10ve@gmail.com>
2021-10-30 09:17:53 +0900

    add endpoint

--- src/router/router.go
+++ src/router/router.go
@@ -41,6 +41,9 @@ func Route() *gin.Engine {
 	r.GET("/plan", con.GetAllPlans)
 	r.GET("/plan/:id", con.GetPlanByID)
 
+	r.GET("/validate/email/:email", con.IsValidEmail)
+	r.GET("/validate/username/:username", con.IsValidUserName)
+
 	r.POST("/register", con.RegisterPost)
 	r.POST("/login", con.LoginPOST)
 	return r
